5fa9d93f0c3ea005d97b25dc250ebd76882088f3
Rosilie Lim <rosilielim@gmail.com>
2023-11-18 15:04:02 -0600

    handled the checkout and cart page  for the logged- in users.

--- carts/models.py
+++ carts/models.py
@@ -4,19 +4,18 @@ from accounts.models import Account
 
 # Create your models here.
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=240, null=True,blank=True)
+    cart_id = models.CharField(max_length=240, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.cart_id
 
-
 class CartItem(models.Model):
     
-    user = models.ForeignKey(Account,on_delete=models.CASCADE,blank=True) # assigns the user to a non-empty cart.
+    user = models.ForeignKey(Account,on_delete=models.CASCADE,blank=True,null=True) # assigns the user to a non-empty cart.
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation,blank=True)
-    cart    = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    cart    = models.ForeignKey(Cart,on_delete=models.CASCADE,null=True,blank=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
